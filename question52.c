28c28dcdc455f74ceb357b5542943352a925d753
Amanbhati9 <bhatiaman699@gmail.com>
2026-06-13 21:53:12 +0530

    question52

---
+++ question52.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Number of Even elements = %d\n", even);
+    printf("Number of Odd elements = %d\n", odd);
+
+    return 0;
+}
